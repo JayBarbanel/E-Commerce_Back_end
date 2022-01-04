0203016ef9ad4fb630cabbe5515050811fb98bbb
Jay <jbarbanel531@gmail.com>
2022-01-04 15:38:34 -0800

    added producttag models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -4,17 +4,35 @@ const sequelize = require('../config/connection');
 
 class ProductTag extends Model {}
 
-ProductTag.init(
-  {
+ProductTag.init({
     // define columns
-  },
-  {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    category_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'product',
+            key: 'id'
+        }
+    },
+    tag_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'tag',
+            key: 'id'
+        }
+    },
+    allowNull: false,
+}, {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'product_tag',
-  }
-);
+});
 
 module.exports = ProductTag;
